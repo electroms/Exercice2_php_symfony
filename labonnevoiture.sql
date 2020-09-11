5af274e95ade8cc84ca080fac11f9bba4a625298
electroms <electroms@users.noreply.github.com>
2020-09-11 16:10:10 +0200

    Définition du readme et ajout de la Bdd dans le projet

---
+++ labonnevoiture.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 11 sep. 2020 à 13:57
+-- Version du serveur :  8.0.18
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `labonnevoiture`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `advert`
+--
+
+DROP TABLE IF EXISTS `advert`;
+CREATE TABLE IF NOT EXISTS `advert` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `advert`
+--
+
+INSERT INTO `advert` (`id`, `name`, `brand_id`) VALUES
+(1, 'Location Renault clio 5', 1),
+(2, 'Vente Citroën C3', 2),
+(3, 'Vente Mercedes Class A', 3),
+(4, 'Location WV T-Roc', 4),
+(5, 'Vente Peugeot 5008', 5),
+(7, 'Location Fiat Vito', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+CREATE TABLE IF NOT EXISTS `brand` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20200911073937', '2020-09-11 07:40:06', 296),
+('DoctrineMigrations\\Version20200911082406', '2020-09-11 08:24:53', 190),
+('DoctrineMigrations\\Version20200911100124', '2020-09-11 10:01:51', 558);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `firstname`, `lastname`, `password`, `roles`) VALUES
+(1, 'mailinpro@gmail.com', 'Pierre-Henry', 'Barge', '$argon2id$v=19$m=65536,t=4,p=1$eC9MQVBnSlVPaUxXUS5NVQ$S2Z6qk9YYmadaCBGzcJlfQ4jqwJxXiXDa02WES6fGc4', '[]');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,16 @@
+## En considérant que vous avez déjà installer symfony et composer : 
+
+#### Tout d'abord après avoir télécharger le projet zipper
+#### Le dézipper dans le répertoire de votre choix
+#### Ouvrir le projet avec votre IDE favori
+#### Dans un terminal éxécuter la commande : composer install
+
+## En considérant que vous possèder un WAMP MAMP ou XAMP :
+
+#### Lancer votre server WAMP
+#### Dans PhpMyAdmin importer la base de donnée se trouvant à la racine du projet (labonnevoiture.sql)
+
+## Si tout c'est bien passé :
+
+#### Exécuter la commande : symfony server:start
+#### Et ouvrer le localhost à l'adresse 127.0.0.1:8000
